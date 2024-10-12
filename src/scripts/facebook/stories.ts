37533b40374d1b8564ea64845dc5eb90a3302234
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-10-12 17:09:04 +0800

    Facebook stories.

--- src/scripts/facebook/stories.ts
+++ src/scripts/facebook/stories.ts
@@ -1,41 +1,26 @@
-let facebookStoriesHidden: boolean = true; // Set default to true (stories hidden)
+const facebookStoriesClass = ".x193iq5w.xgmub6v.x1ceravr";
+let facebookStoriesHidden: boolean = true;
 let facebookStoriesInitialLoad: boolean = true;
 
-// Function to toggle stories visibility
-function toggleStoriesVisibility(): void {
-  facebookStoriesHidden = !facebookStoriesHidden; // Toggle the state
-
-  // Execute the script to hide/show stories in the active tab
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.scripting.executeScript({
-      target: { tabId: tabs[0].id ?? 0 },
-      func: facebookStoriesHidden ? hideFacebookStories : showFacebookStories,
-    });
-  });
-
-  // Update the button text
-  updateButtonText();
-}
-
-// Function to hide Facebook stories
 function hideFacebookStories(): void {
-  const stories = document.querySelectorAll(".x193iq5w.xgmub6v.x1ceravr");
+  // Get the class of the stories from facebook.
+  const stories = document.querySelectorAll(facebookStoriesClass);
+
   stories.forEach((story) => {
-    (story as HTMLElement).style.visibility = "hidden"; // Type assertion
+    (story as HTMLElement).style.visibility = "hidden";
   });
 }
 
-// Function to show Facebook stories
 function showFacebookStories(): void {
-  const stories = document.querySelectorAll(".x193iq5w.xgmub6v.x1ceravr");
+  const stories = document.querySelectorAll(facebookStoriesClass);
+
   stories.forEach((story) => {
-    (story as HTMLElement).style.visibility = "visible"; // Type assertion
+    (story as HTMLElement).style.visibility = "visible";
   });
 }
 
-// Function to update button text based on the current state
 function updateButtonText(): void {
-  const button = document.getElementById("facebook-stories-toggle");
+  const button = document.getElementById("facebook-stories-button");
   if (button) {
     button.innerText = facebookStoriesHidden
       ? "Facebook | Show Stories"
@@ -43,14 +28,33 @@ function updateButtonText(): void {
   }
 }
 
-// Load the initial state when the popup opens.
-document.addEventListener("DOMContentLoaded", () => {
-  updateButtonText(); // Update button text based on the default state
+function toggleFacebookStoriesVisibility(): void {
+  // Reverse the current state.
+  // If true, set to false.
+  // If false, set to true.
+  facebookStoriesHidden = !facebookStoriesHidden;
 
-  // Add click event listener to the button
-  const button = document.getElementById("facebook-stories-toggle");
+  // Execute the script to hide/show stories in the active tab..
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id ?? 0 },
+      func: facebookStoriesHidden ? hideFacebookStories : showFacebookStories,
+    });
+  });
+
+  // Update the button text.
+  updateButtonText();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Update button text based on the default state.
+  updateButtonText();
+
+  // Add click event listener to the button.
+  const button = document.getElementById("facebook-stories-button");
+
   if (button) {
-    button.addEventListener("click", toggleStoriesVisibility);
+    button.addEventListener("click", toggleFacebookStoriesVisibility);
   }
 });
 
