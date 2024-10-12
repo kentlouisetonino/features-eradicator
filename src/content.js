ebd5db3c78e42fa8822ad55672a9fb16447050c7
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-10-12 14:49:12 +0800

    It works now.

---
+++ src/content.js
@@ -0,0 +1,9 @@
+function hideFacebookStories() {
+  const stories = document.querySelectorAll(".x193iq5w.xgmub6v.x1ceravr");
+  stories.forEach((story) => {
+    story.style.visibility = "hidden"; // Hide stories
+  });
+}
+
+// Execute the function when the content script runs
+hideFacebookStories();
