ea7152c8a73a86de18839d10bab28ca2f7fae791
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-10-12 15:07:07 +0800

    Add the dist in gitignore.

--- dist/content.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function hideFacebookStoriesUnique() {
-    const stories = document.querySelectorAll(".x193iq5w.xgmub6v.x1ceravr");
-    stories.forEach((story) => {
-        story.style.visibility = "hidden"; // Hide stories
-    });
-}
-// Execute the function when the content script runs
-hideFacebookStoriesUnique();
